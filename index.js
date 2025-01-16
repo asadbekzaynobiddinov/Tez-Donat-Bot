d0af82ac633b04ab8d7bcd41857492390b01d127
asadbekzaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2025-01-16 20:06:24 +0500

    Update

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/bot', webhookCallback('/bot'));
+app.use('/bot', webhookCallback(bot));
 
 async function bootstrap() {
   await sequelize.authenticate();
   await sequelize.sync();
   app.listen(3001, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3001');
     bot.api.setWebhook('https://bot.takedaservice.uz/bot').then(() => {
       console.log('Webhook is set successfully');
     }).catch((err) => {
