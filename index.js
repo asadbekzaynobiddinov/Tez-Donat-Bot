66ea76eb27bd0be9ffc33c06f4a1abc7918d5db2
asadbekzaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2025-01-16 17:47:06 +0500

    Webhook

---
+++ index.js
@@ -0,0 +1,25 @@
+import { webhookCallback } from 'grammy';
+import express from 'express';
+import sequelize from './src/database/index.js';
+import { bot } from './src/bot/index.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/bot', webhookCallback('/bot'));
+
+async function bootstrap() {
+  await sequelize.authenticate();
+  await sequelize.sync();
+  app.listen(3001, () => {
+    console.log('Server is running on port 3000');
+    bot.api.setWebhook('https://bot.takedaservice.uz/bot').then(() => {
+      console.log('Webhook is set successfully');
+    }).catch((err) => {
+      console.error('Error setting webhook:', err);
+    });
+  });
+}
+
+bootstrap();
