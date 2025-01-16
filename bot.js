7043b8898f332c73b71f36ace7e4169a4c3f2bba
asadbekzaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2025-01-16 17:09:09 +0500

    bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+import { Bot } from "grammy";
+import express, { json } from "express";
+import { webhookCallback } from "grammy";
+
+// Bot yaratish
+const bot = new Bot("8148944761:AAE9RCUzC_nbnBXraN3szMQ3nHnzE4x_gaI");
+
+// Xabarni qayta ishlash
+bot.on("message", (ctx) => {
+  ctx.reply("Salom! Bu serverdagi botdan xabar!");
+});
+
+// HTTP server yaratish
+const app = express();
+
+app.use(json());
+
+// Webhookni sozlash
+app.use("/bot", webhookCallback(bot)); // To‘g‘ridan-to‘g‘ri botni ulash
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server ${PORT}-portda ishga tushdi.`);
+  bot.api.setWebhook(`https://bot.takedaservice.uz/bot`).then(() => {
+    console.log("Webhook muvaffaqiyatli sozlandi!");
+  }).catch((err) => {
+    console.error("Webhookni sozlashda xatolik yuz berdi:", err);
+  });
+});
--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-import { webhookCallback } from 'grammy';
-import express from 'express';
 import sequelize from './src/database/index.js';
 import { bot } from './src/bot/index.js';
 
-const app = express();
-
-app.use(express.json());
-
-app.use(webhookCallback(bot, "express"));
-
 async function bootstrap() {
-  
   await sequelize.authenticate();
   await sequelize.sync();
 
-  const endpoint = 'https://bot.takedaservice.uz'
   bot.start();
   console.log('Webhock was set')
-
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
 }
 
 bootstrap();
