d286d08d03099d9aa5525dcd35f74b8d67cd567c
asadbekzaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2025-01-17 13:36:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-import { webhookCallback } from 'grammy';
-import express from 'express';
+// import { webhookCallback } from 'grammy';
+// import express from 'express';
 import sequelize from './src/database/index.js';
 import { bot } from './src/bot/index.js';
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(webhookCallback(bot, 'express'));
+// app.use(webhookCallback(bot, 'express'));
 
 async function bootstrap() {
   await sequelize.authenticate();
   await sequelize.sync();
-  app.listen(3001, () => {
-    console.log('Server is running on port 3001');
-    bot.api.setWebhook('https://api.takedaservice.uz').then(() => {
-      console.log('Webhook is set successfully');
-    }).catch((err) => {
-      console.error('Error setting webhook:', err);
-    });
-  });
+  // app.listen(3001, () => {
+    // console.log('Server is running on port 3001');
+    // bot.api.setWebhook('https://api.takedaservice.uz').then(() => {
+      // console.log('Webhook is set successfully');
+    // }).catch((err) => {
+      // console.error('Error/ setting webhook:', err);
+    // });
+  // });
+  bot.start()
 }
 bootstrap();
